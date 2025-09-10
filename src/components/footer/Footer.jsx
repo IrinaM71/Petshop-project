6b645c2eb9dc683f19c573242a3ab2968ded378c
IrinaM71 <git@github.com>
2025-09-10 16:40:37 +0200

    Добавила стили для footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,29 +5,31 @@ import WhatsAppIcon from "../../assets/icons/WhatsApp.svg";
 function Footer() {
   return (
     <footer className={styles.footer}>
+      <h2 className={styles.footerTitle}>Contact</h2>
       <div className={styles.contact}>
         <p>
-          Phone <br /> +49 30 915-88492
+          <span>Phone</span> <br /> +49 30 915-88492
         </p>
         <p>
-          Social
+          <span>Socials</span>
           <br />
           <img src={InstagramIcon} alt="Instagram" />
           <img src={WhatsAppIcon} alt="Whatsapp" />
         </p>
         <p>
-          Address <br /> Wallstrasse 9-13, 10179 Berlin, Deutschland
+          <span>Address</span> <br /> Wallstrasse 9-13, 10179 Berlin,
+          Deutschland
         </p>
         <p>
-          Working Hours <br />
+          <span>Working Hours</span> <br />
           24 hours a day
         </p>
       </div>
       <div className={styles.map}>
         <iframe
           src="https://www.google.com/maps/embed?..."
-          width="300"
-          height="200"
+          width="1360"
+          height="350"
           style={{ border: 0 }}
           allowFullScreen=""
           loading="lazy"
